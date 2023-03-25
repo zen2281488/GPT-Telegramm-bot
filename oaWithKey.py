36ad93a2a9bc7059127116e099fa2d4418268567
aecsu4 <aecsu4@omnigroup.su>
2023-03-26 00:06:26 +0300

    first commit

---
+++ oaWithKey.py
@@ -0,0 +1,34 @@
+import requests
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+TOKEN = "Telegramm bot TOKEN"
+url = 'https://api.openai.com/v1/chat/completions'
+key = "API key"
+headers = {'Content-Type': 'application/json', "Authorization": f"Bearer {key}"}
+
+def start_gpt(update, context):
+    text = update.message.text
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": text}]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    if response.ok:
+        response_data = response.json()
+        if 'choices' in response_data and response_data['choices']:
+            message = response_data['choices'][0]['message']
+            if 'content' in message:
+                content = message['content']
+                update.message.reply_text(content)
+    else:
+        update.message.reply_text("Ошибка запроса: {}".format(response.status_code))
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    dp = updater.dispatcher
+    dp.add_handler(MessageHandler(Filters.text, start_gpt))
+    updater.start_polling()
+
+if __name__ == '__main__':
+    main()
