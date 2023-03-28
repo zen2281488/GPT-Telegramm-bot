a8f0146812eddedc32e9e162d0c39c718d17f749
aecsu4 <aecsu4@omnigroup.su>
2023-03-28 21:02:29 +0300

    Context added(without del)

--- oaTGWithKey.py
+++ oaTGWithKey.py
@@ -5,19 +5,23 @@ TOKEN = "Telegramm bot TOKEN"
 url = 'https://api.openai.com/v1/chat/completions'
 key = "API key"
 headers = {'Content-Type': 'application/json', "Authorization": f"Bearer {key}"}
-
+textHistory= []
+textHistoryDict = {"model": "gpt-3.5-turbo","messages":textHistory}
 def start_gpt(update, context):
     text = update.message.text
     data = {
         "model": "gpt-3.5-turbo",
         "messages": [{"role": "user", "content": text}]
     }
-
-    response = requests.post(url, headers=headers, json=data)
+    print(textHistory)
+    textHistory.append({"role": "user", "content": text})
+    response = requests.post(url, headers=headers, json=textHistoryDict)
     if response.ok:
         response_data = response.json()
         if 'choices' in response_data and response_data['choices']:
             message = response_data['choices'][0]['message']
+            textHistory.append(message)
+            # print(message)
             if 'content' in message:
                 content = message['content']
                 update.message.reply_text(content)
